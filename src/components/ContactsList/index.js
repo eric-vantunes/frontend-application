49806fcd07e395cf7136a1af64ceab9da69fe9a8
eric antunes <antunesv.eric@gmail.com>
2022-11-30 13:43:15 -0300

    feat: created contacts list

--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -3,6 +3,8 @@ import {
 } from './styles';
 
 import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export default function ContactList() {
   return (
@@ -26,6 +28,57 @@ export default function ContactList() {
               <strong>Eric Antunes</strong>
               <small>instagram</small>
             </div>
+            <span>antunesv.eric@gmail.com</span>
+            <span>(15)9999-0000</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+
+          <div className="info">
+            <div className="contact-name">
+              <strong>Eric Antunes</strong>
+              <small>instagram</small>
+            </div>
+            <span>antunesv.eric@gmail.com</span>
+            <span>(15)9999-0000</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+
+          <div className="info">
+            <div className="contact-name">
+              <strong>Eric Antunes</strong>
+              <small>instagram</small>
+            </div>
+            <span>antunesv.eric@gmail.com</span>
+            <span>(15)9999-0000</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
           </div>
         </Card>
       </ListContainer>
