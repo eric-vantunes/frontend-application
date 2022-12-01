23c689ef78b62a80b768c9ec7d14af071babe0f6
eric antunes <antunesv.eric@gmail.com>
2022-12-01 12:03:53 -0300

    feat: routes-created

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,26 @@
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 
 import Header from '../Header';
-import ContactList from '../ContactsList';
+import Routes from '../../Routes';
 
 import { Container } from './styles';
 
 function App() {
   return (
+  <BrowserRouter>
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
+
       <Container>
         <Header />
-        <ContactList />
+        <Routes />
       </Container>
     </ThemeProvider>
+  </BrowserRouter>
   );
 }
 
